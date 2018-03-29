3393882dc8254f82fc20449a2f962c41f889687b
Chris Cummer <chriscummer@me.com>
2018-03-29 03:04:43 -0700

    Color 1on1 calendar events differently than other calendar events

--- gcal/client.go
+++ gcal/client.go
@@ -115,8 +115,7 @@ func Fetch() *calendar.Events {
 	}
 
 	t := today().Format(time.RFC3339)
-	events, err := srv.Events.List("primary").ShowDeleted(false).
-		SingleEvents(true).TimeMin(t).MaxResults(10).OrderBy("startTime").Do()
+	events, err := srv.Events.List("primary").ShowDeleted(false).SingleEvents(true).TimeMin(t).MaxResults(10).OrderBy("startTime").Do()
 	if err != nil {
 		log.Fatalf("Unable to retrieve next ten of the user's events. %v", err)
 	}
