ab4774c86a675f4226471c98d981590d12524b87
Chris Cummer <chriscummer@me.com>
2018-03-30 10:10:21 -0700

    Widgets refresh via goroutine
    All widgets now refresh their own data using their own internal go
    routine. This allows them to set their own update schedule (where
    RefreshInterval is the time in seconds between refreshes).
    
    The app uses a goroutine to redraw itself once a second.

--- weather/widget.go
+++ weather/widget.go
@@ -10,16 +10,19 @@ import (
 )
 
 type Widget struct {
-	RefreshedAt time.Time
-	View        *tview.TextView
+	RefreshedAt     time.Time
+	RefreshInterval int
+	View            *tview.TextView
 }
 
 func NewWidget() *Widget {
 	widget := Widget{
-		RefreshedAt: time.Now(),
+		RefreshedAt:     time.Now(),
+		RefreshInterval: 600,
 	}
 
 	widget.addView()
+	go widget.refresher()
 
 	return &widget
 }
@@ -116,6 +119,21 @@ func icon(data *owm.CurrentWeatherData) string {
 	}
 
 	return icon
+}
+
+func (widget *Widget) refresher() {
+	tick := time.NewTicker(time.Duration(widget.RefreshInterval) * time.Second)
+	quit := make(chan struct{})
+
+	for {
+		select {
+		case <-tick.C:
+			widget.Refresh()
+		case <-quit:
+			tick.Stop()
+			return
+		}
+	}
 }
 
 func (widget *Widget) refreshedAt() string {
