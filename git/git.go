d0a05875b66b6040dd15b3288582c4b7ed1c0f17
Chris Cummer <chris.cummer@lendesk.com>
2018-03-31 23:37:56 -0700

    Display current git repo and branch

---
+++ git/git.go
@@ -0,0 +1,16 @@
+package git
+
+import ()
+
+func Fetch() map[string][]string {
+	client := NewClient()
+
+	result := make(map[string][]string)
+
+	result["repo"] = []string{client.Repository}
+	result["branch"] = []string{client.CurrentBranch()}
+	result["changed"] = client.ChangedFiles()
+	result["commits"] = client.Commits()
+
+	return result
+}
