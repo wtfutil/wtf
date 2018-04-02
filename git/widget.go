ccabf5c4117232536823ae0419cb84250b56176e
Chris Cummer <chris.cummer@lendesk.com>
2018-04-01 20:57:55 -0700

    Git now displays changed files and last ten commits

--- git/widget.go
+++ git/widget.go
@@ -2,7 +2,9 @@ package git
 
 import (
 	"fmt"
+	"strings"
 	"time"
+	"unicode/utf8"
 
 	"github.com/rivo/tview"
 	"github.com/senorprogrammer/wtf/wtf"
@@ -48,16 +50,75 @@ func (widget *Widget) addView() {
 	view.SetBorder(true)
 	view.SetDynamicColors(true)
 	view.SetTitle(widget.Name)
+	view.SetWrap(false)
 
 	widget.View = view
 }
 
 func (widget *Widget) contentFrom(data map[string][]string) string {
 	str := "\n"
-	str = str + fmt.Sprintf(" [green]%s[white] [grey]%s[white]\n", data["repo"][0], data["branch"][0])
+	str = str + fmt.Sprintf(" [green]%s[white] [dodgerblue]%s[white]\n", data["repo"][0], data["branch"][0])
+
+	str = str + widget.formatChanges(data["changes"])
 	str = str + "\n"
+	str = str + widget.formatCommits(data["commits"])
 
 	return str
+}
+
+func (widget *Widget) formatChanges(data []string) string {
+	str := ""
+	str = str + " [red]Changed Files[white]\n"
+
+	for _, line := range data {
+		str = str + widget.formatChange(line)
+	}
+
+	return str
+}
+
+func (widget *Widget) formatChange(line string) string {
+	if len(line) == 0 {
+		return ""
+	}
+
+	line = strings.TrimSpace(line)
+	firstChar, _ := utf8.DecodeRuneInString(line)
+
+	// Revisit this and kill the ugly duplication
+	switch firstChar {
+	case 'A':
+		line = strings.Replace(line, "A", "[green]A[white]", 1)
+	case 'D':
+		line = strings.Replace(line, "D", "[red]D[white]", 1)
+	case 'M':
+		line = strings.Replace(line, "M", "[blue]M[white]", 1)
+	case 'R':
+		line = strings.Replace(line, "R", "[purple]R[white]", 1)
+	case '!':
+		line = strings.Replace(line, "!", "[yellow]![white]", 1)
+	case '?':
+		line = strings.Replace(line, "?", "[yellow]?[white]", 1)
+	default:
+		line = line
+	}
+
+	return fmt.Sprintf(" %s\n", strings.Replace(line, "\"", "", -1))
+}
+
+func (widget *Widget) formatCommits(data []string) string {
+	str := ""
+	str = str + " [red]Recent Commits[white]\n"
+
+	for _, line := range data {
+		str = str + widget.formatCommit(line)
+	}
+
+	return str
+}
+
+func (widget *Widget) formatCommit(line string) string {
+	return fmt.Sprintf(" %s\n", strings.Replace(line, "\"", "", -1))
 }
 
 func (widget *Widget) refresher() {
