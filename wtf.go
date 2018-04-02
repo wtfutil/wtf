a4fc98a778401f02f3c7b889e43452b9a303d7b9
Chris Cummer <chris.cummer@lendesk.com>
2018-04-01 22:49:38 -0700

    Add JIRA frame

--- wtf.go
+++ wtf.go
@@ -7,6 +7,7 @@ import (
 	"github.com/senorprogrammer/wtf/bamboohr"
 	"github.com/senorprogrammer/wtf/gcal"
 	"github.com/senorprogrammer/wtf/git"
+	"github.com/senorprogrammer/wtf/jira"
 	"github.com/senorprogrammer/wtf/opsgenie"
 	"github.com/senorprogrammer/wtf/security"
 	"github.com/senorprogrammer/wtf/status"
@@ -38,6 +39,9 @@ func main() {
 	git := git.NewWidget()
 	git.Refresh()
 
+	jira := jira.NewWidget()
+	jira.Refresh()
+
 	opsgenie := opsgenie.NewWidget()
 	opsgenie.Refresh()
 
@@ -51,17 +55,18 @@ func main() {
 	weather.Refresh()
 
 	grid := tview.NewGrid()
-	grid.SetRows(9, 9, 9, 24, 3) // How _high_ the row is, in terminal rows
-	grid.SetColumns(40, 40)      // How _wide_ the column is, in terminal columns
+	grid.SetRows(9, 9, 9, 9, 15, 3) // How _high_ the row is, in terminal rows
+	grid.SetColumns(40, 40)         // How _wide_ the column is, in terminal columns
 	grid.SetBorder(false)
 
 	grid.AddItem(bamboo.View, 0, 0, 2, 1, 0, 0, false)
-	grid.AddItem(cal.View, 2, 0, 2, 1, 0, 0, false)
+	grid.AddItem(cal.View, 2, 0, 3, 1, 0, 0, false)
 	grid.AddItem(git.View, 0, 2, 3, 1, 0, 0, false)
 	grid.AddItem(weather.View, 0, 1, 1, 1, 0, 0, false)
 	grid.AddItem(sec.View, 1, 1, 1, 1, 0, 0, false)
 	grid.AddItem(opsgenie.View, 2, 1, 1, 1, 0, 0, false)
-	grid.AddItem(stat.View, 4, 0, 3, 3, 0, 0, false)
+	grid.AddItem(jira.View, 3, 1, 1, 1, 0, 0, false)
+	grid.AddItem(stat.View, 5, 0, 3, 3, 0, 0, false)
 
 	app := tview.NewApplication()
 
