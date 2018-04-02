3e6c90bf9fee1b8d986716eb86ca8ffd5838e928
Chris Cummer <chris.cummer@lendesk.com>
2018-04-02 08:43:42 -0700

    The /wtf directory is now in source control

---
+++ wtf/utils.go
@@ -0,0 +1,29 @@
+package wtf
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os/exec"
+)
+
+func CenterText(str string, width int) string {
+	return fmt.Sprintf("%[1]*s", -width, fmt.Sprintf("%[1]*s", (width+len(str))/2, str))
+}
+
+func ExecuteCommand(cmd *exec.Cmd) string {
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return fmt.Sprintf("A: %v\n", err)
+	}
+
+	if err := cmd.Start(); err != nil {
+		return fmt.Sprintf("B: %v\n", err)
+	}
+
+	var str string
+	if b, err := ioutil.ReadAll(stdout); err == nil {
+		str += string(b)
+	}
+
+	return str
+}
