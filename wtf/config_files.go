0d547018c33adea6d635b4364091de87c12d79c5
Chris Cummer <chris.cummer@lendesk.com>
2018-04-04 20:02:03 -0700

    Creates the ~/.wtf directory. Displays a message if the config file is missing.

---
+++ wtf/config_files.go
@@ -0,0 +1,35 @@
+package wtf
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/olebedev/config"
+	"github.com/senorprogrammer/wtf/homedir"
+)
+
+// CreateConfigDir creates the .wtf directory in the user's home dir
+func CreateConfigDir() bool {
+	homeDir, _ := homedir.Expand("~/.wtf/")
+
+	if _, err := os.Stat(homeDir); os.IsNotExist(err) {
+		err := os.Mkdir(homeDir, os.ModePerm)
+		if err != nil {
+			panic(err)
+		}
+	}
+	return true
+}
+
+// LoadConfigFile loads the config.yml file to configure the app
+func LoadConfigFile() *config.Config {
+	configPath, _ := homedir.Expand("~/.wtf/config.yml")
+
+	cfg, err := config.ParseYamlFile(configPath)
+	if err != nil {
+		fmt.Println("\n\n\033[1m ERROR:\033[0m Could not load '\033[0;33mconfig.yml\033[0m'.\n Please add a \033[0;33mconfig.yml\033[0m file to your \033[0;33m~/.wtf\033[0m directory.\n See \033[1;34mhttps://github.com/senorprogrammer/wtf\033[0m for details.\n\n")
+		os.Exit(1)
+	}
+
+	return cfg
+}
