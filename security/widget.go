70b56c314ba5958047a323a4b0c0fe4a337ea5fb
Chris Cummer <chriscummer@me.com>
2018-05-31 18:01:11 -0700

    Close #90. Firewall 'off' values are properly formatted

--- security/widget.go
+++ security/widget.go
@@ -46,12 +46,23 @@ func (widget *Widget) contentFrom(data *SecurityData) string {
 	str = str + fmt.Sprintf(" %8s: %s\n", "Network", data.WifiName)
 	str = str + fmt.Sprintf(" %8s: %s\n", "Crypto", data.WifiEncryption)
 	str = str + "\n"
-	str = str + " [red]Firewall[white]       [red]DNS[white]\n"
-	str = str + fmt.Sprintf(" %8s: %4s   %-16s\n", "Enabled", data.FirewallEnabled, data.DnsAt(0))
-	str = str + fmt.Sprintf(" %8s: %4s   %-16s\n", "Stealth", data.FirewallStealth, data.DnsAt(1))
+	str = str + " [red]Firewall[white]        [red]DNS[white]\n"
+	str = str + fmt.Sprintf(" %8s: [%s]%-3s[white]   %-16s\n", "Enabled", widget.labelColor(data.FirewallEnabled), data.FirewallEnabled, data.DnsAt(0))
+	str = str + fmt.Sprintf(" %8s: [%s]%-3s[white]   %-16s\n", "Stealth", widget.labelColor(data.FirewallStealth), data.FirewallStealth, data.DnsAt(1))
 	str = str + "\n"
 	str = str + " [red]Users[white]\n"
 	str = str + fmt.Sprintf(" %s", strings.Join(data.LoggedInUsers, ", "))
 
 	return str
+}
+
+func (widget *Widget) labelColor(label string) string {
+	switch label {
+	case "on":
+		return "green"
+	case "off":
+		return "red"
+	default:
+		return "white"
+	}
 }
