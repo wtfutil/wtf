c2d2197ed0a96fbdda58fc67e7bf023b16f16872
Hossein Mehrabi <ohyeaross@gmail.com>
2018-06-04 05:19:20 +0430

    use map for http response

--- cryptoexchanges/cryptolive/cryptolive.go
+++ cryptoexchanges/cryptolive/cryptolive.go
@@ -15,33 +15,7 @@ type toCurrency struct {
 	price float32
 }
 
-type cResponse struct {
-	BTC  float32 `json:"BTC"`
-	HBZ  float32 `json:"HBZ"`
-	ETH  float32 `json:"ETH"`
-	EOS  float32 `json:"EOS"`
-	BCH  float32 `json:"BCH"`
-	TRX  float32 `json:"TRX"`
-	XRP  float32 `json:"XRP"`
-	LTC  float32 `json:"LTC"`
-	ETC  float32 `json:"ETC"`
-	ADA  float32 `json:"ADA"`
-	CMT  float32 `json:"CMT"`
-	DASH float32 `json:"DASH"`
-	ZEC  float32 `json:"ZEC"`
-	IOT  float32 `json:"IOT"`
-	ONT  float32 `json:"ONT"`
-	NEO  float32 `json:"NEO"`
-	BTG  float32 `json:"BTG"`
-	LSK  float32 `json:"LSK"`
-	ELA  float32 `json:"ELA"`
-	DTA  float32 `json:"DTA"`
-	NANO float32 `json:"NANO"`
-	WTC  float32 `json:"WTC"`
-	DOGE float32 `json:"DOGE"`
-	USD  float32 `json:"USD"`
-	EUR  float32 `json:"EUR"`
-}
+type cResponse map[string]float32
 
 /* -------------------- Unexported Functions -------------------- */
 
