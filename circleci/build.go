994ddf0422111b8841d6682a60c1f31791f721f1
Michael Cordell <surpher@gmail.com>
2018-06-09 07:32:28 -0700

    Add circleci module
    Needs a Circle API token, which can be found at
    https://circleci.com/account/api. This is passed under the environmental
    variable WTF_CIRCLE_API_KEY.

---
+++ circleci/build.go
@@ -0,0 +1,10 @@
+package circleci
+
+type Build struct {
+	AuthorEmail        string            `json:"author_email"`
+	AuthorName         string            `json:"author_name"`
+	Branch             string            `json:"branch"`
+	BuildNum           int               `json:"build_num"`
+	Reponame           string            `json:"reponame"`
+	Status             string            `json:"status"`
+}
