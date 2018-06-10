608815ac855dbfd7287fbc0868d6efd0f5b55f77
Andrew <andrew@andrew>
2018-06-10 03:07:43 +0300

    Fixed name

--- blockfolio/widget.go
+++ blockfolio/widget.go
@@ -7,9 +7,9 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/andrewzolotukhin/wtf/wtf"
 	"github.com/olebedev/config"
 	"github.com/rivo/tview"
+	"github.com/senorprogrammer/wtf/wtf"
 )
 
 // Config is a pointer to the global config object
