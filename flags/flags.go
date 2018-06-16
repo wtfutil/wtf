e92289ebae4f0996094f786d2189969c1c04c777
Chris Cummer <chris.cummer@lendesk.com>
2018-06-16 08:31:11 -0700

    Move command line flags into it's own module

---
+++ flags/flags.go
@@ -0,0 +1,54 @@
+package flags
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	goFlags "github.com/jessevdk/go-flags"
+	"github.com/senorprogrammer/wtf/wtf"
+)
+
+type Flags struct {
+	Config  string `short:"c" long:"config" optional:"yes" description:"Path to config file"`
+	Module  string `short:"m" long:"module" optional:"yes" description:"Display info about a specific module, i.e.: 'wtf -m=todo'"`
+	Version bool   `short:"v" long:"version" description:"Show Version Info"`
+}
+
+func NewFlags() *Flags {
+	flags := Flags{}
+	return &flags
+}
+
+/* -------------------- Exported Functions -------------------- */
+
+func (flags *Flags) HasConfig() bool {
+	return len(flags.Config) > 0
+}
+
+func (flags *Flags) HasModule() bool {
+	return len(flags.Module) > 0
+}
+
+func (flags *Flags) Parse(version string) {
+	parser := goFlags.NewParser(flags, goFlags.Default)
+	if _, err := parser.Parse(); err != nil {
+		if flagsErr, ok := err.(*goFlags.Error); ok && flagsErr.Type == goFlags.ErrHelp {
+			os.Exit(0)
+		}
+	}
+
+	if !flags.HasConfig() {
+		homeDir, err := wtf.Home()
+		if err != nil {
+			os.Exit(1)
+		}
+
+		flags.Config = filepath.Join(homeDir, ".wtf", "config.yml")
+	}
+
+	if flags.Version {
+		fmt.Printf("Version: %s\n", version)
+		os.Exit(0)
+	}
+}
