c64270e6ad8f437d396bd789466ce7bfc2b06d12
Chris Cummer <chris.cummer@lendesk.com>
2018-06-29 12:25:25 -0700

    Wrap Google's calendar Event in a CalEvent struct

--- gcal/widget.go
+++ gcal/widget.go
@@ -5,15 +5,14 @@ import (
 	"time"
 
 	"github.com/senorprogrammer/wtf/wtf"
-	"google.golang.org/api/calendar/v3"
 )
 
 type Widget struct {
 	wtf.TextWidget
 
-	events *calendar.Events
-	ch     chan struct{}
-	mutex  sync.Mutex
+	calEvents []*CalEvent
+	ch        chan struct{}
+	mutex     sync.Mutex
 }
 
 func NewWidget() *Widget {
@@ -29,66 +28,24 @@ func NewWidget() *Widget {
 
 /* -------------------- Exported Functions -------------------- */
 
-func (widget *Widget) Refresh() {
-	events, _ := Fetch()
-	widget.events = events
-
-	widget.UpdateRefreshedAt()
-
-	widget.display()
-}
-
 func (widget *Widget) Disable() {
 	close(widget.ch)
 	widget.TextWidget.Disable()
 }
 
-/* -------------------- Unexported Functions -------------------- */
-
-// conflicts returns TRUE if this event conflicts with another, FALSE if it does not
-func (widget *Widget) conflicts(event *calendar.Event, events *calendar.Events) bool {
-	conflict := false
-
-	for _, otherEvent := range events.Items {
-		if event == otherEvent {
-			continue
-		}
-
-		eventStart, _ := time.Parse(time.RFC3339, event.Start.DateTime)
-		eventEnd, _ := time.Parse(time.RFC3339, event.End.DateTime)
-
-		otherEnd, _ := time.Parse(time.RFC3339, otherEvent.End.DateTime)
-		otherStart, _ := time.Parse(time.RFC3339, otherEvent.Start.DateTime)
-
-		if eventStart.Before(otherEnd) && eventEnd.After(otherStart) {
-			conflict = true
-			break
-		}
-	}
-
-	return conflict
-}
-
-func (widget *Widget) eventIsAllDay(event *calendar.Event) bool {
-	return len(event.Start.Date) > 0
-}
-
-// eventIsNow returns true if the event is happening now, false if it not
-func (widget *Widget) eventIsNow(event *calendar.Event) bool {
-	startTime, _ := time.Parse(time.RFC3339, event.Start.DateTime)
-	endTime, _ := time.Parse(time.RFC3339, event.End.DateTime)
-
-	return time.Now().After(startTime) && time.Now().Before(endTime)
-}
-
-func (widget *Widget) eventIsPast(event *calendar.Event) bool {
-	if widget.eventIsAllDay(event) {
-		return false
+func (widget *Widget) Refresh() {
+	calEvents, err := Fetch()
+	if err != nil {
+		widget.calEvents = []*CalEvent{}
 	} else {
-		ts, _ := time.Parse(time.RFC3339, event.Start.DateTime)
-		return (widget.eventIsNow(event) == false) && ts.Before(time.Now())
+		widget.calEvents = calEvents
 	}
+
+	widget.UpdateRefreshedAt()
+	widget.display()
 }
+
+/* -------------------- Unexported Functions -------------------- */
 
 func updateLoop(widget *Widget) {
 	interval := wtf.Config.UInt("wtf.mods.gcal.textInterval", 30)
