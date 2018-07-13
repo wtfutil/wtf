857702e9710d753ca9771cc8c42c1436c4a6abd5
Chris Cummer <chris.cummer@lendesk.com>
2018-07-12 17:51:23 -0700

    Add specs for wtf.ChecklistItem

--- wtf/checklist_item.go
+++ wtf/checklist_item.go
@@ -1,10 +1,13 @@
 package wtf
 
+// ChecklistItem is a module for creating generic checklist implementations
+// See 'Todo' for an implementation example
 type ChecklistItem struct {
 	Checked bool
 	Text    string
 }
 
+// CheckMark returns the string used to indicate a ChecklistItem is checked or unchecked
 func (item *ChecklistItem) CheckMark() string {
 	if item.Checked {
 		return Config.UString("wtf.mods.todo.checkedIcon", "x")
@@ -13,6 +16,8 @@ func (item *ChecklistItem) CheckMark() string {
 	}
 }
 
+// Toggle changes the checked state of the ChecklistItem
+// If checked, it is unchecked. If unchecked, it is checked
 func (item *ChecklistItem) Toggle() {
 	item.Checked = !item.Checked
 }
