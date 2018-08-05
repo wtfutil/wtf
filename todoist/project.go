07d9bb5e99f0f9a25c0b1d0dd4ccb23c288f65ce
Chris Cummer <chriscummer@me.com>
2018-08-05 11:10:02 -0700

    Don't die if Todoist can't connect to the API

--- todoist/project.go
+++ todoist/project.go
@@ -14,10 +14,12 @@ type Project struct {
 }
 
 func NewProject(id int) *Project {
+  // Todoist seems to experience a lot of network issues on their side
+  // If we can't connect, handle it with an empty project until we can
 	project, err := todoist.GetProject(id)
-	if err != nil {
-		panic(err)
-	}
+  if err != nil {
+    return &Project{}
+  }
 
 	proj := &Project{
 		Project: project,
