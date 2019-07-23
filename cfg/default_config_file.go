0bc696568147d333bf7051f18cf1ff3a3bf7683f
Chris Cummer <chriscummer@me.com>
2019-07-22 22:54:01 -0700

    WTF-504 Improve the default config

--- cfg/default_config_file.go
+++ cfg/default_config_file.go
@@ -1,17 +1,36 @@
 package cfg
 
-const defaultConfigFile = `wtf:
+const defaultConfigFile = `
+wtf:
   colors:
     border:
       focusable: darkslateblue
       focused: orange
       normal: gray
   grid:
-    columns: [40, 40]
-    rows: [13, 13, 4]
+    columns: [32, 32, 32, 32]
+    rows: [10, 10, 10, 4, 4]
   refreshInterval: 1
   mods:
-    clocks:
+    clocks_a:
+      colors:
+        rows:
+          even: "lightblue"
+          odd: "white"
+      enabled: true
+      locations:
+        Vancouver: "America/Vancouver"
+        Toronto: "America/Toronto"
+      position:
+        top: 0
+        left: 1
+        height: 1
+        width: 1
+      refreshInterval: 15
+      sort: "alphabetical"
+      title: "Clocks A"
+      type: "clocks"
+    clocks_b:
       colors:
         rows:
           even: "lightblue"
@@ -21,46 +40,66 @@ const defaultConfigFile = `wtf:
         Avignon: "Europe/Paris"
         Barcelona: "Europe/Madrid"
         Dubai: "Asia/Dubai"
-        Vancouver: "America/Vancouver"
-        Toronto: "America/Toronto"
       position:
         top: 0
-        left: 0
+        left: 2
         height: 1
         width: 1
       refreshInterval: 15
       sort: "alphabetical"
-    security:
+      title: "Clocks B"
+      type: "clocks"
+    feedreader:
       enabled: true
+      feeds:
+      - http://wtfutil.com/blog/index.xml
+      feedLimit: 10
       position:
         top: 1
-        left: 0
+        left: 1
+        width: 2
         height: 1
-        width: 1
-      refreshInterval: 3600
-    status:
+      updateInterval: 14400
+    ipinfo:
+      colors:
+        name: "lightblue"
+        value: "white"
       enabled: true
       position:
         top: 2
-        left: 0
-        height: 1
-        width: 2
-      refreshInterval: 1
-    system:
-      enabled: true
-      position:
-        top: 0
         left: 1
         height: 1
         width: 1
-      refreshInterval: 3600
+      refreshInterval: 150
+    power:
+      enabled: true
+      position:
+        top: 2
+        left: 2
+        height: 1
+        width: 1
+      refreshInterval: 15
+      title: "⚡️"
     textfile:
       enabled: true
       filePath: "~/.config/wtf/config.yml"
+      format: true
       position:
-        top: 1
-        left: 1
-        height: 1
+        top: 0
+        left: 0
+        height: 4
         width: 1
       refreshInterval: 30
+      wrapText: false
+    uptime:
+      args: [""]
+      cmd: "uptime"
+      enabled: true
+      position:
+        top: 3
+        left: 1
+        height: 1
+        width: 2
+      refreshInterval: 30
+      type: cmdrunner
 `
