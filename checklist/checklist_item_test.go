e1ad1f3065de32efae0b08c0491c65629998f215
Chris Cummer <chriscummer@me.com>
2019-08-04 08:42:32 -0700

    Simplify ChecklistItem tests

---
+++ checklist/checklist_item_test.go
@@ -0,0 +1,36 @@
+package checklist
+
+import (
+	"testing"
+
+	. "github.com/stretchr/testify/assert"
+)
+
+func testChecklistItem() *ChecklistItem {
+	item := NewChecklistItem(
+		false,
+		"test",
+		"x",
+		" ",
+	)
+	return item
+}
+
+func Test_CheckMark(t *testing.T) {
+	item := testChecklistItem()
+	Equal(t, " ", item.CheckMark())
+
+	item.Toggle()
+	Equal(t, "x", item.CheckMark())
+}
+
+func Test_Toggle(t *testing.T) {
+	item := testChecklistItem()
+	Equal(t, false, item.Checked)
+
+	item.Toggle()
+	Equal(t, true, item.Checked)
+
+	item.Toggle()
+	Equal(t, false, item.Checked)
+}
