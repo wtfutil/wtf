f56cc483b4538d8ad2c827c5a9d2b4a6f6a776c5
Jason Schweier <jasons@nulogy.com>
2019-10-03 10:44:48 -0400

    Add Buildkite widget

---
+++ modules/buildkite/settings.go
@@ -0,0 +1,54 @@
+package buildkite
+
+import (
+	"github.com/olebedev/config"
+	"github.com/wtfutil/wtf/cfg"
+	"github.com/wtfutil/wtf/utils"
+	"os"
+)
+
+type PipelineSettings struct {
+	slug     string
+	branches []string
+}
+
+type Settings struct {
+	common    *cfg.Common
+	token     string             `help:"Your Buildkite API Token"`
+	orgSlug   string             `help:"Organization Slug"`
+	pipelines []PipelineSettings `help:"An array of pipelines to get data from"`
+}
+
+const defaultTitle = "Buildkite"
+const defaultFocusable = true
+
+func NewSettingsFromYAML(name string, ymlConfig *config.Config, globalConfig *config.Config) *Settings {
+	settings := Settings{
+		common:    cfg.NewCommonSettingsFromModule(name, defaultTitle, defaultFocusable, ymlConfig, globalConfig),
+		token:     ymlConfig.UString("token", os.Getenv("WTF_BUILDKITE_TOKEN")),
+		orgSlug:   ymlConfig.UString("organizationSlug"),
+		pipelines: buildPipelineSettings(ymlConfig),
+	}
+
+	return &settings
+}
+
+func buildPipelineSettings(ymlConfig *config.Config) []PipelineSettings {
+	pipelines := []PipelineSettings{}
+
+	for slug, _ := range ymlConfig.UMap("pipelines") {
+		branches := utils.ToStrs(ymlConfig.UList("pipelines." + slug + ".branches"))
+		if len(branches) == 0 {
+			branches = []string{"master"}
+		}
+
+		pipeline := PipelineSettings{
+			slug:     slug,
+			branches: branches,
+		}
+
+		pipelines = append(pipelines, pipeline)
+	}
+
+	return pipelines
+}
