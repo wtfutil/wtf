38593e32701de34ecd882f843ad4a455cfed7054
Chris Cummer <chriscummer@me.com>
2019-10-29 23:07:04 -0700

    WTF-657 Add spec coverage for cfg/position_validation.go
    Signed-off-by: Chris Cummer <chriscummer@me.com>

---
+++ cfg/position_validation_test.go
@@ -0,0 +1,26 @@
+package cfg
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/alecthomas/assert"
+)
+
+var (
+	posVal = &positionValidation{
+		err:    errors.New("Busted"),
+		name:   "top",
+		intVal: -3,
+	}
+)
+
+func Test_Attributes(t *testing.T) {
+	assert.EqualError(t, posVal.Error(), "Busted")
+	assert.Equal(t, true, posVal.HasError())
+	assert.Equal(t, -3, posVal.IntValue())
+
+	assert.Contains(t, posVal.String(), "Invalid")
+	assert.Contains(t, posVal.String(), "top")
+	assert.Contains(t, posVal.String(), "-3")
+}
