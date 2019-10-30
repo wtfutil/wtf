86069d2dc002666b8afc5ee4859c75188cc03b94
Chris Cummer <chriscummer@me.com>
2019-10-30 14:55:20 -0700

    WTF-657 Add spec coverage for utils/text.go
    Signed-off-by: Chris Cummer <chriscummer@me.com>

--- utils/text_test.go
+++ utils/text_test.go
@@ -3,18 +3,26 @@ package utils
 import (
 	"testing"
 
-	. "github.com/stretchr/testify/assert"
+	"github.com/rivo/tview"
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_CenterText(t *testing.T) {
-	Equal(t, "cat", CenterText("cat", -9))
-	Equal(t, "cat", CenterText("cat", 0))
-	Equal(t, "   cat   ", CenterText("cat", 9))
+	assert.Equal(t, "cat", CenterText("cat", -9))
+	assert.Equal(t, "cat", CenterText("cat", 0))
+	assert.Equal(t, "   cat   ", CenterText("cat", 9))
+}
+
+func Test_HighlightableHelper(t *testing.T) {
+	view := tview.NewTextView()
+	actual := HighlightableHelper(view, "cats", 0, 5)
+
+	assert.Equal(t, "[\"0\"][\"\"]cats           [\"\"]\n", actual)
 }
 
 func Test_RowPadding(t *testing.T) {
-	Equal(t, "", RowPadding(0, 0))
-	Equal(t, "", RowPadding(5, 2))
-	Equal(t, " ", RowPadding(1, 2))
-	Equal(t, "     ", RowPadding(0, 5))
+	assert.Equal(t, "", RowPadding(0, 0))
+	assert.Equal(t, "", RowPadding(5, 2))
+	assert.Equal(t, " ", RowPadding(1, 2))
+	assert.Equal(t, "     ", RowPadding(0, 5))
 }
