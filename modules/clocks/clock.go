fcc2a211dfa421344367bb2d3830dd62b3f4b6aa
Sean Smith <seandanielsmith@gmail.com>
2020-10-03 13:10:50 -0400

    Add natural ordering for clocks
    This turns locations into an ordered array, so we can have a 'natural' ordering
    This is backwards compatible
    Resolves #896

--- modules/clocks/clock.go
+++ modules/clocks/clock.go
@@ -1,6 +1,7 @@
 package clocks
 
 import (
+	"strings"
 	"time"
 )
 
@@ -18,6 +19,14 @@ func NewClock(label string, timeLoc *time.Location) Clock {
 	return clock
 }
 
+func BuildClock(label string, location string) (clock Clock, err error) {
+	timeLoc, err := time.LoadLocation(sanitizeLocation(location))
+	if err != nil {
+		return Clock{}, err
+	}
+	return NewClock(label, timeLoc), nil
+}
+
 func (clock *Clock) Date(dateFormat string) string {
 	return clock.LocalTime().Format(dateFormat)
 }
@@ -32,4 +41,8 @@ func (clock *Clock) ToLocal(t time.Time) time.Time {
 
 func (clock *Clock) Time(timeFormat string) string {
 	return clock.LocalTime().Format(timeFormat)
+}
+
+func sanitizeLocation(locStr string) string {
+	return strings.Replace(locStr, " ", "_", -1)
 }
