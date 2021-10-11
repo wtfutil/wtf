d038aecb9202ad719ce72dfe0d478dbabfe3fa60
Brian Choromanski <BrianChoromanski@gmail.com>
2021-10-11 10:08:25 -0400

    Fixed golint error

--- modules/ipaddresses/ipapi/widget.go
+++ modules/ipaddresses/ipapi/widget.go
@@ -6,12 +6,12 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-	"text/template"
 	"strings"
+	"text/template"
 
 	"github.com/rivo/tview"
-	"github.com/wtfutil/wtf/view"
 	"github.com/wtfutil/wtf/utils"
+	"github.com/wtfutil/wtf/view"
 )
 
 // Widget widget struct
@@ -23,46 +23,46 @@ type Widget struct {
 }
 
 type ipinfo struct {
-	Query          string  `json:"query"`
-	ISP            string  `json:"isp"`
-	AS             string  `json:"as"`
-	ASName         string  `json:"asname"`
-	District       string  `json:"district"`
-	City           string  `json:"city"`
-	Region         string  `json:"region"`
-	RegionName     string  `json:"regionName"`
-	Country        string  `json:"country"`
-	CountryCode    string  `json:"countryCode"`
-	Continent      string  `json:"continent"`
-	ContinentCode  string  `json:"continentCode"`
-	Latitude       float64 `json:"lat"`
-	Longitude      float64 `json:"lon"`
-	PostalCode     string  `json:"zip"`
-	Currency       string  `json:"currency"`
-	Organization   string  `json:"org"`
-	Timezone       string  `json:"timezone"`
-	ReverseDNS     string  `json:"reverse"`
+	Query         string  `json:"query"`
+	ISP           string  `json:"isp"`
+	AS            string  `json:"as"`
+	ASName        string  `json:"asname"`
+	District      string  `json:"district"`
+	City          string  `json:"city"`
+	Region        string  `json:"region"`
+	RegionName    string  `json:"regionName"`
+	Country       string  `json:"country"`
+	CountryCode   string  `json:"countryCode"`
+	Continent     string  `json:"continent"`
+	ContinentCode string  `json:"continentCode"`
+	Latitude      float64 `json:"lat"`
+	Longitude     float64 `json:"lon"`
+	PostalCode    string  `json:"zip"`
+	Currency      string  `json:"currency"`
+	Organization  string  `json:"org"`
+	Timezone      string  `json:"timezone"`
+	ReverseDNS    string  `json:"reverse"`
 }
 
 var argLookup = map[string]string{
-	"ip":             "IP Address",
-	"isp":            "ISP",
-	"as":             "AS",
-	"asname":         "AS Name",
-	"district":       "District",
-	"city":           "City",
-	"region":         "Region",
-	"regionname":     "Region Name",
-	"country":        "Country",
-	"countrycode":    "Country Code",
-	"continent":      "Continent",
-	"continentcode":  "Continent Code",
-	"coordinates":    "Coordinates",
-	"postalcode":     "Postal Code",
-	"currency":       "Currency",
-	"organization":   "Organization",
-	"timezone":       "Timezone",
-	"reversedns":     "Reverse DNS",
+	"ip":            "IP Address",
+	"isp":           "ISP",
+	"as":            "AS",
+	"asname":        "AS Name",
+	"district":      "District",
+	"city":          "City",
+	"region":        "Region",
+	"regionname":    "Region Name",
+	"country":       "Country",
+	"countrycode":   "Country Code",
+	"continent":     "Continent",
+	"continentcode": "Continent Code",
+	"coordinates":   "Coordinates",
+	"postalcode":    "Postal Code",
+	"currency":      "Currency",
+	"organization":  "Organization",
+	"timezone":      "Timezone",
+	"reversedns":    "Reverse DNS",
 }
 
 // NewWidget constructor
@@ -115,14 +115,14 @@ func (widget *Widget) setResult(info *ipinfo) {
 	args := utils.ToStrs(widget.settings.args)
 
 	// if no arguments are defined set default
-	if len(args) == 0{
+	if len(args) == 0 {
 		args = []string{"ip", "isp", "as", "city", "region", "country", "coordinates", "postalCode", "organization", "timezone"}
 	}
 
 	format := ""
 
-	for _, arg := range args{
-		if val, ok := argLookup[strings.ToLower(arg)]; ok{
+	for _, arg := range args {
+		if val, ok := argLookup[strings.ToLower(arg)]; ok {
 			format = format + formatableText(val, strings.ToLower(arg))
 		}
 	}
@@ -132,26 +132,26 @@ func (widget *Widget) setResult(info *ipinfo) {
 	resultBuffer := new(bytes.Buffer)
 
 	err := resultTemplate.Execute(resultBuffer, map[string]string{
-		"nameColor":      widget.settings.colors.name,
-		"valueColor":     widget.settings.colors.value,
-		"ip":             info.Query,
-		"isp":            info.ISP,
-		"as":             info.AS,
-		"asname":         info.ASName,
-		"district":       info.District,
-		"city":           info.City,
-		"region":         info.Region,
-		"regionname":     info.RegionName,
-		"country":        info.Country,
-		"countrycode":    info.CountryCode,
-		"continent":      info.Continent,
-		"continentcode":  info.ContinentCode,
-		"coordinates":    strconv.FormatFloat(info.Latitude, 'f', 6, 64) + "," + strconv.FormatFloat(info.Longitude, 'f', 6, 64),
-		"postalcode":     info.PostalCode,
-		"currency":       info.Currency,
-		"organization":   info.Organization,
-		"timezone":       info.Timezone,
-		"reversedns":     info.ReverseDNS,
+		"nameColor":     widget.settings.colors.name,
+		"valueColor":    widget.settings.colors.value,
+		"ip":            info.Query,
+		"isp":           info.ISP,
+		"as":            info.AS,
+		"asname":        info.ASName,
+		"district":      info.District,
+		"city":          info.City,
+		"region":        info.Region,
+		"regionname":    info.RegionName,
+		"country":       info.Country,
+		"countrycode":   info.CountryCode,
+		"continent":     info.Continent,
+		"continentcode": info.ContinentCode,
+		"coordinates":   strconv.FormatFloat(info.Latitude, 'f', 6, 64) + "," + strconv.FormatFloat(info.Longitude, 'f', 6, 64),
+		"postalcode":    info.PostalCode,
+		"currency":      info.Currency,
+		"organization":  info.Organization,
+		"timezone":      info.Timezone,
+		"reversedns":    info.ReverseDNS,
 	})
 
 	if err != nil {
