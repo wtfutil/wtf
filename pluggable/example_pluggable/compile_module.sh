9cc6943246250eada7e63be1b6aa245c669bbefc
James Alavosus <alavosus.james@gmail.com>
2021-10-12 05:20:02 -0400

    add github.com/hashicorp/go-plugin

--- pluggable/example_pluggable/compile_module.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-go build -buildmode=plugin -o ./example_pluggable.so ./example_pluggable.go
--- pluggable/example_pluggable/example_pluggable_main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-// This file is included to prevent CI from breaking,
-// while also being the easy/scaredy-cat way out of making CI config changes.
-
-func main() {
-
-}
