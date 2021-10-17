0c0ea1a28abaa40160bd9807f72cdc780a869e47
Yoshihisa Mochihara <motihara@gmail.com>
2021-10-17 05:24:42 +0200

    Not showing wifi info when no wifi (#1124) (#1126)

--- modules/security/widget.go
+++ modules/security/widget.go
@@ -40,10 +40,14 @@ func (widget *Widget) Refresh() {
 func (widget *Widget) content() (string, string, bool) {
 	data := NewSecurityData()
 	data.Fetch()
-	str := fmt.Sprintf(" [%s]WiFi[white]\n", widget.settings.Colors.Subheading)
-	str += fmt.Sprintf(" %8s: %s\n", "Network", data.WifiName)
-	str += fmt.Sprintf(" %8s: %s\n", "Crypto", data.WifiEncryption)
-	str += "\n"
+	var str string
+
+	if data.WifiName != "" {
+		str += fmt.Sprintf(" [%s]WiFi[white]\n", widget.settings.Colors.Subheading)
+		str += fmt.Sprintf(" %8s: %s\n", "Network", data.WifiName)
+		str += fmt.Sprintf(" %8s: %s\n", "Crypto", data.WifiEncryption)
+		str += "\n"
+	}
 
 	str += fmt.Sprintf(" [%s]Firewall[white]\n", widget.settings.Colors.Subheading)
 	str += fmt.Sprintf(" %8s: %4s\n", "Status", data.FirewallEnabled)
