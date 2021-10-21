e30a5ca4d8c549993ce775f19dff557e6b5ef24d
Yoshihisa Mochihara <motihara@gmail.com>
2021-10-21 07:53:21 +0200

    Fix digitalocean Image / Region information bug (#1132)
    Correct ref(address) is not passed to StringValueForProperty before.
    Send correct ref as an argument to StringValueForProperty

--- modules/digitalocean/droplet.go
+++ modules/digitalocean/droplet.go
@@ -1,8 +1,6 @@
 package digitalocean
 
 import (
-	"fmt"
-	"reflect"
 	"strings"
 
 	"github.com/digitalocean/godo"
@@ -13,68 +11,33 @@ import (
 type Droplet struct {
 	godo.Droplet
 
-	Image  Image
-	Region Region
-}
-
-// Image represents WTF's view of a DigitalOcean droplet image
-type Image struct {
-	godo.Image
-	utils.Reflective
-}
-
-// Region represents WTF's view of a DigitalOcean region
-type Region struct {
-	godo.Region
-	utils.Reflective
+	Image  godo.Image
+	Region godo.Region
 }
 
 // NewDroplet creates and returns an instance of Droplet
 func NewDroplet(doDroplet godo.Droplet) *Droplet {
-	droplet := &Droplet{
+	return &Droplet{
 		doDroplet,
-
-		Image{
-			*doDroplet.Image,
-			utils.Reflective{},
-		},
-
-		Region{
-			*doDroplet.Region,
-			utils.Reflective{},
-		},
+		*doDroplet.Image,
+		*doDroplet.Region,
 	}
-
-	return droplet
 }
 
 /* -------------------- Exported Functions -------------------- */
 
 // StringValueForProperty returns a string value for the given column
 func (drop *Droplet) StringValueForProperty(propName string) (string, error) {
-	var strVal string
-	var err error
-
 	// Figure out if we should forward this property to a sub-object
 	// Lets us support "Region.Name" column definitions
 	split := strings.Split(propName, ".")
 
 	switch split[0] {
 	case "Image":
-		strVal, err = drop.Image.StringValueForProperty(split[1])
+		return utils.StringValueForProperty(drop.Image, split[1])
 	case "Region":
-		strVal, err = drop.Region.StringValueForProperty(split[1])
+		return utils.StringValueForProperty(drop.Region, split[1])
 	default:
-		v := reflect.ValueOf(drop)
-		refVal := reflect.Indirect(v).FieldByName(propName)
-
-		if !refVal.IsValid() {
-			err = fmt.Errorf("invalid property name: %s", propName)
-		} else {
-			strVal = fmt.Sprintf("%v", refVal)
-		}
-
+		return utils.StringValueForProperty(drop, propName)
 	}
-
-	return strVal, err
 }
--- utils/reflective.go
+++ utils/reflective.go
@@ -5,13 +5,9 @@ import (
 	"reflect"
 )
 
-// Reflective is a convenience wrapper for objects that makes it possible to
-// extract property values from the object by property name
-type Reflective struct{}
-
 // StringValueForProperty returns a string value for the given property
 // If the property doesn't exist, it returns an error
-func (ref *Reflective) StringValueForProperty(propName string) (string, error) {
+func StringValueForProperty(ref interface{}, propName string) (string, error) {
 	v := reflect.ValueOf(ref)
 	refVal := reflect.Indirect(v).FieldByName(propName)
 
