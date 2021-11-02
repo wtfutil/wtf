1d568833d469a9afbb0c83d76b19bd376e213fe7
Chris Cummer <chriscummer@me.com>
2021-11-02 16:51:59 -0700

    Unescape escaped HTML entities in FeedReader item titles (#1143)
    Signed-off-by: Chris Cummer <chriscummer@me.com>

---
+++ modules/feedreader/widget_test.go
@@ -0,0 +1,64 @@
+package feedreader
+
+import (
+	"testing"
+
+	"github.com/mmcdole/gofeed"
+	"gotest.tools/assert"
+)
+
+func Test_getShowText(t *testing.T) {
+	tests := []struct {
+		name     string
+		feedItem *FeedItem
+		showType ShowType
+		expected string
+	}{
+		{
+			name:     "with nil FeedItem",
+			feedItem: nil,
+			showType: SHOW_TITLE,
+			expected: "",
+		},
+		{
+			name: "with plain title",
+			feedItem: &FeedItem{
+				item: &gofeed.Item{Title: "Cats and Dogs"},
+			},
+			showType: SHOW_TITLE,
+			expected: "Cats and Dogs",
+		},
+		{
+			name: "with escaped title",
+			feedItem: &FeedItem{
+				item: &gofeed.Item{Title: "&lt;Cats and Dogs&gt;"},
+			},
+			showType: SHOW_TITLE,
+			expected: "<Cats and Dogs>",
+		},
+		{
+			name: "with unescaped title",
+			feedItem: &FeedItem{
+				item: &gofeed.Item{Title: "<Cats and Dogs>"},
+			},
+			showType: SHOW_TITLE,
+			expected: "<Cats and Dogs>",
+		},
+		{
+			name: "with link",
+			feedItem: &FeedItem{
+				item: &gofeed.Item{Title: "Cats and Dogs", Link: "https://cats.com/dog.xml"},
+			},
+			showType: SHOW_LINK,
+			expected: "https://cats.com/dog.xml",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actual := getShowText(tt.feedItem, tt.showType)
+
+			assert.Equal(t, tt.expected, actual)
+		})
+	}
+}
