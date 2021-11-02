dd3ed7b93529c29257195d81fab07e4db2c5d988
Igor Zibarev <hypnoglow@gmail.com>
2021-11-03 01:48:41 +0300

    [kubernetes] use k8s client per widget (#1141)
    References issue #1081

--- modules/kubernetes/client.go
+++ modules/kubernetes/client.go
@@ -1,17 +1,11 @@
 package kubernetes
 
 import (
-	"sync"
-
 	"k8s.io/client-go/kubernetes"
 	// Includes authentication modules for various Kubernetes providers
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	"k8s.io/client-go/tools/clientcmd"
 )
-
-var kubeClient *clientInstance
-var kubeError error
-var clientOnce sync.Once
 
 type clientInstance struct {
 	Client kubernetes.Interface
@@ -19,18 +13,14 @@ type clientInstance struct {
 
 // getInstance returns a Kubernetes interface for a clientset
 func (widget *Widget) getInstance() (*clientInstance, error) {
-	clientOnce.Do(func() {
-		if kubeClient == nil {
-			client, err := widget.getKubeClient()
-			if err != nil {
-				kubeError = err
-			}
-			kubeClient = &clientInstance{
-				Client: client,
-			}
-		}
+	var err error
+
+	widget.clientOnce.Do(func() {
+		widget.client = &clientInstance{}
+		widget.client.Client, err = widget.getKubeClient()
 	})
-	return kubeClient, kubeError
+
+	return widget.client, err
 }
 
 // getKubeClient returns a kubernetes clientset for the kubeconfig provided
