1017c6710dcd721d9d6cfe2015b038ac90e57d4a
Sean Smith <seandanielsmith@gmail.com>
2022-08-23 00:09:39 -0400

    Fix tag filtering
    Addresses #1172

--- modules/todo/display.go
+++ modules/todo/display.go
@@ -90,13 +90,16 @@ func (widget *Widget) shouldShowItem(item *checklist.ChecklistItem) bool {
 
 	for _, tag := range item.Tags {
 		for _, hideTag := range widget.settings.hideTags {
-			if (widget.showTagPrefix == "" && tag == hideTag) || !strings.HasPrefix(tag, widget.showTagPrefix) {
+			if (widget.showTagPrefix == "" && tag == hideTag) {
 				return false
 			}
 		}
+		if (widget.showTagPrefix == "" || strings.HasPrefix(tag, widget.showTagPrefix)) {
+			return true
+		}
 	}
 
-	return true
+	return false
 }
 
 func (widget *Widget) RowColor(idx int, hidden int, checked bool) string {
@@ -213,3 +216,4 @@ func getNowDate() time.Time {
 	now = time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.Now().Location())
 	return now
 }
+
