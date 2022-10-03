d4b4fccf0356c4bc21afe50fd33ee298c131ea6f
Albert Fung <Bizzaro@users.noreply.github.com>
2022-10-03 10:30:13 -0400

    add config options for digital clock (#1353)

--- modules/digitalclock/settings.go
+++ modules/digitalclock/settings.go
@@ -14,10 +14,12 @@ const (
 type Settings struct {
 	*cfg.Common
 
-	color      string `help:"The color of the clock."`
-	font       string `help:"The font of the clock." values:"bigfont or digitalfont"`
-	hourFormat string `help:"The format of the clock." values:"12 or 24"`
-	withDate   bool   `help:"Whether or not to display date information"`
+	color          string `help:"The color of the clock."`
+	font           string `help:"The font of the clock." values:"bigfont or digitalfont"`
+	hourFormat     string `help:"The format of the clock." values:"12 or 24"`
+	dateFormat     string `help:"The format of the date."`
+	withDate       bool   `help:"Whether or not to display date information"`
+	withDatePrefix bool   `help:"Whether or not to display Date: prefix"`
 }
 
 // NewSettingsFromYAML creates a new settings instance from a YAML config block
@@ -25,10 +27,12 @@ func NewSettingsFromYAML(name string, ymlConfig *config.Config, globalConfig *co
 	settings := Settings{
 		Common: cfg.NewCommonSettingsFromModule(name, defaultTitle, defaultFocusable, ymlConfig, globalConfig),
 
-		color:      ymlConfig.UString("color"),
-		font:       ymlConfig.UString("font"),
-		hourFormat: ymlConfig.UString("hourFormat", "24"),
-		withDate:   ymlConfig.UBool("withDate", true),
+		color:          ymlConfig.UString("color"),
+		font:           ymlConfig.UString("font"),
+		hourFormat:     ymlConfig.UString("hourFormat", "24"),
+		dateFormat:     ymlConfig.UString("dateFormat", "Monday January 02 2006"),
+		withDate:       ymlConfig.UBool("withDate", true),
+		withDatePrefix: ymlConfig.UBool("withDatePrefix", true),
 	}
 
 	return &settings
