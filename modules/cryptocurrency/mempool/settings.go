3bd1b1bcb1da5fd6a22a7d1cced315761608b145
Chris Cummer <chriscummer@me.com>
2022-10-04 19:40:56 -0700

    gofmt a file
    Signed-off-by: Chris Cummer <chriscummer@me.com>

--- modules/cryptocurrency/mempool/settings.go
+++ modules/cryptocurrency/mempool/settings.go
@@ -14,15 +14,15 @@ const (
 type Settings struct {
 	common *cfg.Common
 
-    // Define your settings attributes here
+	// Define your settings attributes here
 }
 
 // NewSettingsFromYAML creates a new settings instance from a YAML config block
 func NewSettingsFromYAML(name string, ymlConfig *config.Config, globalConfig *config.Config) *Settings {
 	settings := Settings{
-        common: cfg.NewCommonSettingsFromModule(name, defaultTitle, defaultFocusable, ymlConfig, globalConfig),
+		common: cfg.NewCommonSettingsFromModule(name, defaultTitle, defaultFocusable, ymlConfig, globalConfig),
 
-        // Configure your settings attributes here. See http://github.com/olebedev/config for type details
+		// Configure your settings attributes here. See http://github.com/olebedev/config for type details
 	}
 
 	return &settings
