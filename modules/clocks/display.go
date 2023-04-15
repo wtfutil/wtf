e42daca1897deba1ef61c83d091e2235d91149f1
Sean Smith <seandanielsmith@gmail.com>
2023-04-15 10:14:15 -0500

    Make width of location dynamic, to accomodate longer labels
    This addresses #1553

--- modules/clocks/display.go
+++ modules/clocks/display.go
@@ -5,13 +5,21 @@ import "fmt"
 func (widget *Widget) display(clocks []Clock, dateFormat string, timeFormat string) {
 	str := ""
 
+	locationWidth := 12
+	for _, clock := range clocks {
+		if len(clock.Label) > locationWidth {
+			locationWidth = len(clock.Label) + 2
+		}
+	}
+
 	if len(clocks) == 0 {
 		str = fmt.Sprintf("\n%s", " no timezone data available")
 	} else {
 		for idx, clock := range clocks {
 			str += fmt.Sprintf(
-				" [%s]%-12s %-10s %7s[white]\n",
+				" [%s]%-*s %-10s %7s[white]\n",
 				widget.CommonSettings().RowColor(idx),
+				locationWidth,
 				clock.Label,
 				clock.Time(timeFormat),
 				clock.Date(dateFormat),
