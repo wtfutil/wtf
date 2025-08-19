b5e8b72fe45dde4ad864f99de0ee1b5011aa7be9
Darren Cauthon <darren@cauthon.com>
2025-08-19 07:46:11 -0500

    Formatting.

--- modules/cmdrunner/settings.go
+++ modules/cmdrunner/settings.go
@@ -15,13 +15,13 @@ const (
 type Settings struct {
 	*cfg.Common
 
-	args       []string `help:"The arguments to the command, with each item as an element in an array. Example: for curl -I cisco.com, the arguments array would be ['-I', 'cisco.com']."`
-	cmd        string   `help:"The terminal command to be run, withouth the arguments. Ie: ping, whoami, curl."`
-	tail       bool     `help:"Automatically scroll to the end of the command output."`
-	pty        bool     `help:"Run the command in a pseudo-terminal. Some apps will behave differently if they feel in a terminal. For example, some apps will produce colorized output in a terminal, and non-colorized output otherwise. Default false" optional:"true"`
-	ptySuppressErrors bool `help:"Do not display pty errors. Some apps producing colorized output may result in trailing errors. This will attempt to hide them, use only when necessary. Default false" optional:"true"`
-	maxLines   int      `help:"Maximum number of lines kept in the buffer."`
-	workingDir string   `help:"Working directory for command to run in" optional:"true"`
+	args              []string `help:"The arguments to the command, with each item as an element in an array. Example: for curl -I cisco.com, the arguments array would be ['-I', 'cisco.com']."`
+	cmd               string   `help:"The terminal command to be run, withouth the arguments. Ie: ping, whoami, curl."`
+	tail              bool     `help:"Automatically scroll to the end of the command output."`
+	pty               bool     `help:"Run the command in a pseudo-terminal. Some apps will behave differently if they feel in a terminal. For example, some apps will produce colorized output in a terminal, and non-colorized output otherwise. Default false" optional:"true"`
+	ptySuppressErrors bool     `help:"Do not display pty errors. Some apps producing colorized output may result in trailing errors. This will attempt to hide them, use only when necessary. Default false" optional:"true"`
+	maxLines          int      `help:"Maximum number of lines kept in the buffer."`
+	workingDir        string   `help:"Working directory for command to run in" optional:"true"`
 
 	// The dimensions of the module
 	width  int
@@ -33,13 +33,13 @@ func NewSettingsFromYAML(name string, moduleConfig *config.Config, globalConfig 
 	settings := Settings{
 		Common: cfg.NewCommonSettingsFromModule(name, defaultTitle, defaultFocusable, moduleConfig, globalConfig),
 
-		args:       utils.ToStrs(moduleConfig.UList("args")),
-		workingDir: moduleConfig.UString("workingDir", "."),
-		cmd:        moduleConfig.UString("cmd"),
-		pty:        moduleConfig.UBool("pty", false),
+		args:              utils.ToStrs(moduleConfig.UList("args")),
+		workingDir:        moduleConfig.UString("workingDir", "."),
+		cmd:               moduleConfig.UString("cmd"),
+		pty:               moduleConfig.UBool("pty", false),
 		ptySuppressErrors: moduleConfig.UBool("ptySuppressErrors", false),
-		tail:       moduleConfig.UBool("tail", false),
-		maxLines:   moduleConfig.UInt("maxLines", 256),
+		tail:              moduleConfig.UBool("tail", false),
+		maxLines:          moduleConfig.UInt("maxLines", 256),
 	}
 
 	width, height, err := utils.CalculateDimensions(moduleConfig, globalConfig)
