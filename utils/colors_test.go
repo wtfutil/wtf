de342caae494530876b6a1e5401fc6f9ac6f64c8
huyz <huyz@users.noreply.github.com>
2026-05-05 13:42:40 -0500

    Colors utils: add unit tests

--- utils/colors_test.go
+++ utils/colors_test.go
@@ -41,3 +41,44 @@ func Test_ColorizePercent(t *testing.T) {
 		})
 	}
 }
+
+func Test_ColorizeUptimePercent(t *testing.T) {
+	tests := []struct {
+		name     string
+		percent  float64
+		expected string
+	}{
+		{
+			name:     "with excellent uptime",
+			percent:  99,
+			expected: "[green]99[white]",
+		},
+		{
+			name:     "with good uptime",
+			percent:  95,
+			expected: "[yellow]95[white]",
+		},
+		{
+			name:     "with acceptable uptime",
+			percent:  80,
+			expected: "[orange]80[white]",
+		},
+		{
+			name:     "with poor uptime",
+			percent:  50,
+			expected: "[red]50[white]",
+		},
+		{
+			name:     "with negative percent",
+			percent:  -1,
+			expected: "[grey]-1[white]",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actual := ColorizeUptimePercent(tt.percent)
+			assert.Equal(t, tt.expected, actual)
+		})
+	}
+}
